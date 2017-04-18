1b02b7b97de324fb81206cfe43b47b69442d013f
Johnny Wu <KarasAya@gmail.com>
2017-04-18 16:59:34 +0800

    update

--- examples/renderer.js
+++ examples/renderer.js
@@ -15,31 +15,54 @@ let regl = renderer._regl;
 let nodes = [];
 let scene = null;
 
-regltf.load(regl, './assets_01/scene.gltf', (err, result) => {
-  console.log(result);
-  scene = result;
+function _buildNodes(scene) {
+  let nodes = [];
 
-  // init scene
   scene.nodes.forEach(node => {
     let flats = utils.flat(node);
     nodes = nodes.concat(flats);
   });
+
+  return nodes;
+}
+
+
+let gltfPath = '/Users/johnny/jwu/gltf-exporter/Assets/gltf-exports/scene.gltf';
+// let gltfPath = './assets_02/scene.gltf';
+regltf.load(regl, gltfPath, (err, result) => {
+  console.log(result);
+  scene = result;
+
+  // init scene
+  nodes = _buildNodes(scene);
 });
 
 // frame
 shell.frame(() => {
+  regltf.reset();
   lstats.tick();
 
   if (scene) {
+    if (scene._dirty) {
+      nodes = _buildNodes(scene);
+      scene._dirty = false;
+    }
+
     nodes.forEach(node => {
-      // renderer.drawNode(node);
+
+      renderer.drawNode(node);
 
       if (!node._meshes) {
         return;
       }
 
       node._meshes.forEach(id => {
-        let gltfMesh = scene.json.meshes[id];
+        let gltfMesh = scene.meshes[id];
+        if (!gltfMesh) {
+          // console.warn('mesh data not ready');
+          return;
+        }
+
         gltfMesh.primitives.forEach(gltfPrimitive => {
           let data = regltf.buildCommandData(scene, node, gltfPrimitive);
           let cmd = scene.commands[data.techID];
